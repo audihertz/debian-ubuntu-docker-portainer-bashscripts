e86664acda80017de06b3abd17d0dee3a7a9782d
John Bollwitt <126220408+audihertz@users.noreply.github.com>
2023-07-09 15:53:16 -0700

    Update install-portainer.sh

--- install-portainer.sh
+++ install-portainer.sh
@@ -2,8 +2,6 @@
 #
 # https://docs.portainer.io/start/install-ce/server/docker/linux 
 #
-#sudo docker volume create portainer_data \
-#
 sudo docker run -d \
   -p 8000:8000 \
   -p 9443:9443 \
@@ -12,5 +10,5 @@ sudo docker run -d \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /mnt/VOLUMES/_PORTAINER:/data \
-  portainer/portainer-ce:latest \
+  portainer/portainer-ce:latest 
 #
