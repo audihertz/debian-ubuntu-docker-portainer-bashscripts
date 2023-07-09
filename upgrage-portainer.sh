8696da62df5ca8986f94851f01581a75f62d2c0f
John Bollwitt <126220408+audihertz@users.noreply.github.com>
2023-07-09 15:55:39 -0700

    Update upgrage-portainer.sh

--- upgrage-portainer.sh
+++ upgrage-portainer.sh
@@ -8,5 +8,13 @@ sudo docker rm portainer \
 #
 sudo docker pull portainer/portainer-ce:latest \
 #
-sudo docker run -d -p 8000:8000 -p 9443:9443 -e TZ=America/Vancouver --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/VOLUMES/_PORTAINER:/data portainer/portainer-ce:latest \
+sudo docker run -d \
+  -p 8000:8000 \
+  -p 9443:9443 \
+  -e TZ=America/Los_Angeles \
+  --name portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /mnt/VOLUMES/_PORTAINER:/data \
+  portainer/portainer-ce:latest
 #
