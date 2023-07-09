4676a9807b4f1177408d7855cefc71e1164bb892
John Bollwitt <126220408+audihertz@users.noreply.github.com>
2023-07-09 16:05:36 -0700

    Update install-portainer.sh

--- install-portainer.sh
+++ install-portainer.sh
@@ -2,13 +2,17 @@
 #
 # https://docs.portainer.io/start/install-ce/server/docker/linux 
 #
+# Set variables
+timezone="America/Vancouver"
+volume_path="/mnt/VOLUMES/_PORTAINER"
+#
 sudo docker run -d \
   -p 8000:8000 \
   -p 9443:9443 \
-  -e TZ=America/Los_Angeles \
+  -e TZ="$timezone" \
   --name portainer \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /mnt/VOLUMES/_PORTAINER:/data \
+  -v "$volume_path":/data \
   portainer/portainer-ce:latest 
 #
