2529193e42d7eb719e1a015ee7fe67f00311ccf1
John Bollwitt <126220408+audihertz@users.noreply.github.com>
2023-07-09 16:09:44 -0700

    Update upgrage-portainer.sh

--- upgrage-portainer.sh
+++ upgrage-portainer.sh
@@ -2,19 +2,27 @@
 #
 # https://docs.portainer.io/start/upgrade/docker 
 #
+# SET VARIABLES
+timezone="America/Vancouver"
+volume_path="/mnt/VOLUMES/_PORTAINER"
+#
+# STOP PORTAINER
 sudo docker stop portainer \
 #
+# REMOVE PORTAINER
 sudo docker rm portainer \
 #
+# PULL LATEST IMAGE OF PORTAINER
 sudo docker pull portainer/portainer-ce:latest \
 #
+# START PORTAINER AGAIN
 sudo docker run -d \
   -p 8000:8000 \
   -p 9443:9443 \
-  -e TZ=America/Los_Angeles \
+  -e TZ="$timezone" \
   --name portainer \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /mnt/VOLUMES/_PORTAINER:/data \
+  -v "$volume_path":/data \
   portainer/portainer-ce:latest
 #
